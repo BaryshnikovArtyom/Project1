15c9fc773c290cc88630f9f1a9cf2de6c8c65cfb
BaryshnikovArtyom <kmay3123@gmail.com>
2024-11-18 22:53:15 +0300

    Соединение суммы и произведения

--- Задание 1/Program.cs
+++ Задание 1/Program.cs
@@ -3,9 +3,15 @@ int a = int.Parse(Console.ReadLine());
 Console.Write("b = ");
 int b = int.Parse(Console.ReadLine());
 
+// Вычисляем сумму
 int sum = a + b;
 Console.WriteLine($"{a} + {b} = {sum}");
 
+// Вычисляем произведение
+int product = a * b;
+Console.WriteLine($"{a} * {b} = {product}");
+
+// Находим наибольшее число
 if (a > b)
 {
     Console.WriteLine($"Наибольшее число: {a}");
