3bdc54091258d3f414ab56da9b15a9e5caabcfb5
ArtyomBaryshnikov <saominamisa@gmail.com>
2024-11-18 22:31:39 +0300

    Добавьте файлы проекта.

---
+++ Задание 1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("a = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+int b = int.Parse(Console.ReadLine());
+int product = a * b;
+Console.WriteLine($"{a} * {b} = {product}");
